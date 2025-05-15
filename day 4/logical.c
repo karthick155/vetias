d26aeb887270536db66e410b0588ca375524b310
karthick155 <karthickthiyagu2002@gmail.com>
2025-05-15 15:52:35 +0530

    Add files via upload

---
+++ day 4/logical.c
@@ -0,0 +1,8 @@
+| Operation       | Symbol | Description                         | Example                                 |     |            |         |
+| --------------- | ------ | ----------------------------------- | --------------------------------------- | --- | ---------- | ------- |
+| **AND**         | `&`    | Output is 1 if both inputs are 1    | `1 & 1 = 1`, `1 & 0 = 0`                |     |            |         |
+| **OR**          | \`     | \`                                  | Output is 1 if at least one input is 1  | \`1 | 0 = 1`, `0 | 0 = 0\` |
+| **XOR**         | `^`    | Output is 1 if inputs are different | `1 ^ 0 = 1`, `1 ^ 1 = 0`                |     |            |         |
+| **NOT**         | `~`    | Inverts the input                   | `~1 = 0`, `~0 = 1`                      |     |            |         |
+| **Left Shift**  | `<<`   | Shifts bits left (multiplies by 2)  | `0001 0100 << 2 = 0101 0000` (20 -> 80) |     |            |         |
+| **Right Shift** | `>>`   | Shifts bits right (divides by 2)    | `0001 0100 >> 2 = 0000 0101` (20 -> 5)  |     |            |         |
